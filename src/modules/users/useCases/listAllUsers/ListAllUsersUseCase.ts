feadced39841eea66cf42eb12a1b281bf0949f7a
Isaque Igor <isaqueigorsilva@gmail.com>
2022-01-26 19:51:00 -0300

    feat: implement usecases

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,12 @@
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
-interface IRequest {
-  user_id: string;
-}
-
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
+  execute(): User[] {
     // Complete aqui
+    return this.usersRepository.list();
   }
 }
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 import { createUserController } from "../modules/users/useCases/createUser";
 import { listAllUsersController } from "../modules/users/useCases/listAllUsers";
@@ -7,20 +7,20 @@ import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin
 
 const usersRoutes = Router();
 
-usersRoutes.post("/", (request, response) =>
-  createUserController.handle(request, response)
-);
+usersRoutes.post("/", (request: Request, response: Response) => {
+  return createUserController.handle(request, response);
+});
 
-usersRoutes.patch("/:user_id/admin", (request, response) =>
-  turnUserAdminController.handle(request, response)
-);
+usersRoutes.patch("/:user_id/admin", (request: Request, response: Response) => {
+  return turnUserAdminController.handle(request, response);
+});
 
-usersRoutes.get("/:user_id", (request, response) =>
-  showUserProfileController.handle(request, response)
-);
+usersRoutes.get("/:user_id", (request: Request, response: Response) => {
+  return showUserProfileController.handle(request, response);
+});
 
-usersRoutes.get("/", (request, response) =>
-  listAllUsersController.handle(request, response)
-);
+usersRoutes.get("/", (request: Request, response: Response) => {
+  return listAllUsersController.handle(request, response);
+});
 
 export { usersRoutes };
